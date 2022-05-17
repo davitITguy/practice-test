924d50ec0d8416ab1cc14b25da49273fb3e4fae5
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-05-18 02:31:12 +0400

    usergreeting update

--- src/componentsAll/AllComponents.jsx
+++ src/componentsAll/AllComponents.jsx
@@ -68,12 +68,29 @@ class AllComponents extends React.Component {
       );
     };
 
+    function UserGreeting(props) {
+      return <h1>Welcome back!</h1>;
+    }
+
+    function GuestGreeting(props) {
+      return <h1>Please sign up.</h1>;
+    }
+
+    function Greeting(props) {
+      const isLoggedIn = props.isLoggedIn;
+      if (isLoggedIn) {
+        return <UserGreeting />;
+      }
+      return <GuestGreeting />;
+    }
+
     return (
       <div>
         {<p>{element}</p>}
         <div>{elementt}</div>
         <button onClick={activateLasers}>Activate Lasers</button>
         <div>{Form()}</div>
+        <div>{}</div>
       </div>
     );
   }
